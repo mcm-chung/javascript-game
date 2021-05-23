4a6818da9d7f51df6275c5cbdd50f82126933b96
May Chung <maychung@mays-mbp.home>
2021-05-23 16:50:37 +0100

    added a deck of cards

--- main.js
+++ main.js
@@ -3,16 +3,38 @@
 //variables
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let deck = [];
 
 let playerHand = [];
 let dealerHand = [];
 let playerScore = 0;
 let dealerScore = 0;
 
-//classes
+//building a deck of cards
 class Card {
   constructor(suit, value) {
     this.suit = suit;
     this.value = value;
   }
 }
+
+//empty array to contain the cards
+class Deck {
+  constructor(deck) {
+    this.deck = [];
+  }
+}
+
+//create deck
+for (let i = 0; i < suits.length; i++) {
+  for (let x = 0; x < values.length; x++) {
+    let card = { Value: values[x], Suit: suits[i] };
+    deck.push(card);
+  }
+}
+
+console.log(deck);
+
+// createDeck(suit,value) {
+
+// }
