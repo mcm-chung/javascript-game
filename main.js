3d9cb22ae3c2a9d5f5c3f66f6e5a0d0c6b6025fb
May Chung <maychung@mays-mbp.home>
2021-05-24 00:30:20 +0100

    added shuffle method part1

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-// elements in variables - query selector
+// DOM variables:
+// const startButton = document.querySelector(".start");
+// const hitButton = document.querySelector(".hit");
+// const standButton = document.querySelector(".stand");
 
-//variables
+// Card variables
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-let deck = [];
 
+//Game variables
+let deck = [];
 let playerHand = [];
 let dealerHand = [];
 let playerScore = 0;
 let dealerScore = 0;
+let gameStart = false;
+let gameOver = false;
 
 //building a deck of cards
 class Card {
@@ -18,23 +24,28 @@ class Card {
   }
 }
 
-//empty array to contain the cards
-class Deck {
-  constructor(deck) {
-    this.deck = [];
-  }
-}
-
-//create deck
+//create deck with suit and values
 for (let i = 0; i < suits.length; i++) {
   for (let x = 0; x < values.length; x++) {
-    let card = { Value: values[x], Suit: suits[i] };
+    let card = { value: values[x], suit: suits[i] };
     deck.push(card);
   }
 }
 
-console.log(deck);
+// console.log(deck);
 
-// createDeck(suit,value) {
+//shuffle method
+function shuffle(deck) {
+  for (let i = 0; i < 1000; i++) {
+    let location1 = Math.floor(Math.random() * deck.length);
+    let location2 = Math.floor(Math.random() * deck.length);
 
-// }
+    let temp = deck[location1];
+    deck[location1] = deck[location2];
+    deck[location2] = temp;
+  }
+}
+
+console.log(shuffle(deck));
+
+// console.log(deck);
