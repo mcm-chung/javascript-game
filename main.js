0efd04217db209f1270b55100e6468e931421f2a
May Chung <maychung@mays-mbp.home>
2021-05-23 11:31:59 +0100

    added in global/local variables

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
+// elements in variables - query selector
+
+//variables
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let playerHand = [];
+let dealerHand = [];
+let playerScore = 0;
+let dealerScore = 0;
+
+//classes
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
