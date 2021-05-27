40bd6d4185d216dfd13011a0607a3cf97811e5a0
May Chung <maychung@mays-mbp.home>
2021-05-27 15:42:04 +0100

    added refresh feature

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const startButton = document.querySelector(".start");
 const hitButton = document.querySelector(".hit");
 const standButton = document.querySelector(".stand");
-// const pointPlayerDisplay = document.querySelector("#player-results");
+
 // Card variables
 var suits = ["spade", "diamond", "club", "heart"];
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
@@ -48,7 +48,6 @@ for (let i = 0; i < suits.length; i++) {
   }
 }
 console.log(deck);
-// console.log(deck);
 
 //shuffle method
 // function shuffle() {
@@ -71,9 +70,7 @@ function chooseCard() {
   const firstDeckHalf = deck.slice(0, choice);
   const secondDeckHalf = deck.slice(choice + 1);
   deck = firstDeckHalf.concat(secondDeckHalf);
-  // console.log(deck.length);
-  // console.log(deck[choice]);
-  // console.log(cardChoice);
+
   return cardChoice;
 }
 const card = chooseCard();
@@ -131,7 +128,42 @@ const getCardTotal = (deck) => {
   return points;
 };
 
+function checkEndOfGame(playerPoints, dealerPoints) {
+  if (playerPoints > 21) {
+    const dealerWins = document.querySelector(".dealerWins");
+    dealerWins.innerHTML = "Dealer Wins";
+  }
+  if (dealerPoints > 21) {
+    const playerWins = document.querySelector(".playerWins");
+    playerWins.innerHTML = "Player Wins";
+  }
+  if (
+    (dealerPoints < 22 && dealerPoints > playerPoints) ||
+    dealerPoints == 21
+  ) {
+    const dealerWins = document.querySelector(".dealerWins");
+    dealerWins.innerHTML = "Dealer Wins";
+  } else if (playerPoints > dealerPoints) {
+    const playerWins = document.querySelector(".playerWins");
+    playerWins.innerHTML = "Player Wins";
+  } else if (playerPoints == dealerPoints) {
+    const draw = document.querySelector(".draw");
+    document.querySelector(".draw").appendChild(draw);
+  }
+}
+
+const hitGameOverCheck = (playerPoints) => {
+  if (playerPoints > 21) {
+    const dealerWins = document.querySelector(".dealerWins");
+    dealerWins.innerHTML = "Dealer Wins";
+  } else if (playerPoints == 21) {
+    const playerWins = document.querySelector(".playerWins");
+    dealerWins.innerHTML = "Player Wins";
+  }
+};
+
 hitButton.addEventListener("click", () => {
+  // checkEndOfGame();
   if (playerHand.length < 5) {
     const playerCard = addCard(player);
     // const card = chooseCard();
@@ -143,17 +175,22 @@ hitButton.addEventListener("click", () => {
     // addCard(player);
     console.log(playerHand);
   }
-
+  hitGameOverCheck(getCardTotal(playerHand));
   document.getElementById("player-results").innerHTML = `${getCardTotal(
     playerHand
   )} `;
 });
 
 standButton.addEventListener("click", (e) => {
-  checkEndOfGame();
+  checkEndOfGame(getCardTotal(playerHand), getCardTotal(dealerHand));
   getCardTotal(dealerHand);
   console.log(getCardTotal(dealerHand));
-  gameOver = true;
+  if (getCardTotal(dealerHand) < 17) {
+    if (dealerHand.length < 5) {
+      const addDealerCard = addCard(dealer);
+      dealerHand.push(addDealerCard);
+    }
+  }
 
   document.getElementById("dealer-results").innerHTML = `${getCardTotal(
     dealerHand
@@ -162,22 +199,3 @@ standButton.addEventListener("click", (e) => {
 
 document.getElementById("dealer-results").innerHTML = `${dealerPoints}`;
 document.getElementById("player-results").innerHTML = `${playerPoints}`;
-
-dealerWins = "Dealer Wins";
-
-function checkEndOfGame() {
-  if (dealerPoints <= 16) {
-    if (dealerHand.length < 5) {
-      const addDealerCard = addCard(dealer);
-      dealerHand.push(addDealerCard);
-    }
-  }
-
-  if (playerPoints > 21 || dealerPoints > playerPoints) {
-    document.querySelector(".dealerWins").innerHTML = `${dealerWins}`;
-  } else if (playerPoints > dealerPoints) {
-    document.querySelector(".dealerWins").innerHTML = `${dealerWins}`;
-  } else if ((playerPoints = dealerPoints)) {
-    document.querySelector(".draw").innerHTML = `${draw}`;
-  }
-}
